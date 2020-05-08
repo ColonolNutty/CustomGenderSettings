76438b1ee095a6012b477d2890b7236003d0f2cd
ColonolNutty <ColonolNutty@hotmail.com>
2020-05-08 12:11:09 -0600

    Move scripts up a folder

--- Scripts/CGS/cncustomgendersettings/mod_integration/mod_settings_manager/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This file is part of the Custom Gender Settings mod licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International public license (CC BY-NC-ND 4.0).
-https://creativecommons.org/licenses/by-nc-nd/4.0/
-https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
-
-Copyright (c) COLONOLNUTTY
-"""
