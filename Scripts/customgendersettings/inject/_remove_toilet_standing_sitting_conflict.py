69ca0e08e9f8defc4bf3535a2efd86aa41b6319a
ColonolNutty <ColonolNutty@hotmail.com>
2022-08-25 15:12:36 -0600

    Updates:
    - CGS now enables the can and cannot reproduce pet traits for Fox Sims.
    - Removed the included mod_data folder because it is pointless now.

--- Scripts/customgendersettings/inject/_remove_toilet_standing_sitting_conflict.py
+++ Scripts/customgendersettings/inject/_remove_toilet_standing_sitting_conflict.py
@@ -6,6 +6,7 @@ https://creativecommons.org/licenses/by/4.0/legalcode
 Copyright (c) COLONOLNUTTY
 """
 from customgendersettings.modinfo import ModInfo
+from sims.sim_info_types import Species
 from sims4.resources import Types
 from sims4communitylib.enums.traits_enum import CommonTraitId
 from sims4communitylib.events.event_handling.common_event_registry import CommonEventRegistry
@@ -15,7 +16,7 @@ from traits.traits import Trait
 
 
 @CommonEventRegistry.handle_events(ModInfo.get_identity())
-def _cgs_allow_toilet_standing_sitting_at_same_time_on_zone_early_load(event_data: S4CLZoneEarlyLoadEvent):
+def _cgs_update_traits_on_zone_early_load(event_data: S4CLZoneEarlyLoadEvent):
     if event_data.game_loaded:
         # If the game is loaded already, it means we have already done this.
         return True
@@ -26,3 +27,11 @@ def _cgs_allow_toilet_standing_sitting_at_same_time_on_zone_early_load(event_dat
     # noinspection PyTypeChecker
     toilet_sitting_trait: Trait = CommonResourceUtils.load_instance(Types.TRAIT, CommonTraitId.GENDER_OPTIONS_TOILET_SITTING)
     toilet_sitting_trait.conflicting_traits = tuple()
+
+    # noinspection PyTypeChecker
+    can_reproduce_trait: Trait = CommonResourceUtils.load_instance(Types.TRAIT, CommonTraitId.PREGNANCY_OPTIONS_PET_CAN_REPRODUCE)
+    can_reproduce_trait.species = tuple(can_reproduce_trait.species) + (Species.FOX,)
+
+    # noinspection PyTypeChecker
+    cannot_reproduce_trait: Trait = CommonResourceUtils.load_instance(Types.TRAIT, CommonTraitId.PREGNANCY_OPTIONS_PET_CAN_NOT_REPRODUCE)
+    cannot_reproduce_trait.species = tuple(cannot_reproduce_trait.species) + (Species.FOX,)
